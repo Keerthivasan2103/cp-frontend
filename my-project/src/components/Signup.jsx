2f984aa9a748e9d48c86c0d8c980de6b9086e7fe
kavin-lefty <kavinkumarsksi@gmail.com>
2024-08-13 12:36:05 +0530

    KK

--- my-project/src/components/Signup.jsx
+++ my-project/src/components/Signup.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const Signup = () => {
+export function SignUp() {
+  const url = import.meta.env.BASE_URL;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post().then().catch() 
+  };
   return (
-    <div>Signup</div>
-  )
+    <>
+      <section>
+        <div className="flex justify-center items-center h-screen">
+          <div className="home">
+            <form action="" onSubmit={handleSubmit}>
+              <label htmlFor="userName">UserName</label>
+              <input type="text" name="userName" id="userName" />
+              <label htmlFor="email">email</label>
+              <input type="text" name="email" id="email" />
+              <label htmlFor="phoneNumber">Phone Number</label>
+              <input type="text" name="phoneNumber" id="phoneNumber" />
+              <label htmlFor="password">Password</label>
+              <input type="text" name="password" id="password" />
+              <button>Register</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-
-export default Signup
