cc6996518baf38c605ca739084d867c433e476fc
Blackdevil-03 <vasan77492@gmail.com>
2024-08-13 12:18:24 +0530

    com

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Login from './components/Login'
+import Signup from'./components/Signup'
+import Home from './components/Home'
+import {Routes,Route} from "react-router-dom"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-9xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/Signup" element={<Signup/>}></Route>
+      <Route path="/Home" element={<Home/>}></Route>
+    </Routes>
+    
+  
+    </>
+    
   )
 }
 
